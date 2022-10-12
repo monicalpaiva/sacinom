e66aef6a80c8545d0a4114894675b576dcffdf91
monica <monicalpaiva@hotmail.com>
2022-10-12 13:08:14 -0300

    #Personalizando a pagina SUMARIO

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,9 +3,9 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Constants from 'expo-constants';
 import Header from '../templates/header';
+import Navegacao from '../templates/navegacao';
 import Conteudo from '../templates/conteudo';
 import Rodape from '../templates/rodape';
-import Navegacao from '../templates/navegacao';
 
 export default function HomeScreen({navigation}) {
   return (
--- screens/Sumario.js
+++ screens/Sumario.js
@@ -3,18 +3,32 @@ import { StyleSheet, View, Text } from 'react-native';
 
 export default function Sumario() {
   return (
-    <View style={styles.conteudoCentral}>
-     <Text> Sumario</Text>
+    <View style={styles.conteudo}>
+      <View  style={styles.conteudoCentral}>
+        <Text> Nome do autor</Text>
+        <Text> Imagem do autor</Text>
+      </View>
+      <View style={styles.conteudoSobre}>
+        <Text>Descrição do autor</Text>
+        <Text>Links com os títulos para as produções do autor</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  conteudoCentral:{
+  conteudo:{
     flex: 1,
     height:100,
     padding:5,
     backgroundColor: '#fafad2',
+  },
+  conteudoCentral:{
     alignItems: 'center',
+  },
+  conteudoSobre:{
+    height:100,
+    padding:5,
+    alignItems: 'flex-start',
   }
 });
